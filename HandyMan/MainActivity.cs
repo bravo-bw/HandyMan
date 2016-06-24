9477a931fef8c1fcb50052bbd5390e121a398654
Coulson <ckgathi@bhp.org.bw>
2016-06-25 01:15:46 +0200

    initial commit

---
+++ HandyMan/MainActivity.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Threading;
+
+namespace HandyMan
+{
+	[Activity(Label = "HandyMan", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		private Button mButtonSignUp;
+		private Button mButtonSignIn;
+		private ProgressBar mProgressBar;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+			mButtonSignUp = FindViewById<Button>(Resource.Id.btnSignUp);
+			mButtonSignIn = FindViewById<Button>(Resource.Id.btnSignIn);
+			mProgressBar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+
+			mButtonSignUp.Click += (object sender, EventArgs args) =>
+			{
+				//Pull up dialog
+				FragmentTransaction transaction = FragmentManager.BeginTransaction();
+				dialog_SuignUp signUpDialog = new dialog_SuignUp();
+				signUpDialog.Show(transaction, "dialog frangment");
+
+				signUpDialog.mOnSignUpComplete += signUpDialog_mOnSignUpComplete;
+			};
+
+			mButtonSignIn.Click += (object sender, EventArgs args) =>
+			{
+				//Pull up dialog
+				FragmentTransaction transaction = FragmentManager.BeginTransaction();
+				dialog_SuignIn signInDialog = new dialog_SuignIn();
+				signInDialog.Show(transaction, "dialog frangment");
+
+				signInDialog.mOnSignInComplete += signInDialog_mOnSignInComplete;
+			};
+		}
+		void signUpDialog_mOnSignUpComplete(object sender, OnSignUpEventArgs e)
+		{
+			mProgressBar.Visibility = ViewStates.Visible;
+			Thread thread = new Thread(ActLikeARequest);
+			thread.Start();
+		}
+
+		void signInDialog_mOnSignInComplete(object sender, OnSignInEventArgs e)
+		{
+			mProgressBar.Visibility = ViewStates.Visible;
+			Thread thread = new Thread(ActLikeARequest);
+			thread.Start();
+		}
+
+		private void ActLikeARequest()
+		{
+			Thread.Sleep(3000);
+
+			RunOnUiThread(() => { mProgressBar.Visibility = ViewStates.Invisible; });
+		}
+	}
+}
+
+
---
+++ HandyMan/dialog_SignIn.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HandyMan
+{
+	public class OnSignInEventArgs : EventArgs
+	{
+		private string mUserName;
+		private string mPassword;
+
+		public string UserName
+		{
+			get { return mUserName; }
+			set { mUserName = value; }
+		}
+
+		public string Password
+		{
+			get { return mPassword; }
+			set { mPassword = value; }
+		}
+
+		public OnSignInEventArgs(string userName, string password) : base()
+		{
+			UserName = userName;
+			Password = password;
+		}
+	}
+
+	public class dialog_SuignIn : DialogFragment
+	{
+		private EditText mTxtUserName;
+		private EditText mTxtPassword;
+		private Button mBtnSignIn;
+
+		public event EventHandler<OnSignInEventArgs> mOnSignInComplete;
+
+		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			base.OnCreateView(inflater, container, savedInstanceState);
+
+			var view = inflater.Inflate(Resource.Layout.dialog_sign_in, container, false);
+
+			mTxtUserName = view.FindViewById<EditText>(Resource.Id.txtUserName);
+			mTxtPassword = view.FindViewById<EditText>(Resource.Id.txtPassword);
+			mBtnSignIn = view.FindViewById<Button>(Resource.Id.btnDialogSignIn);
+
+			mBtnSignIn.Click += mBtnSignIn_Click;
+
+			return view;
+		}
+
+		void mBtnSignIn_Click(object sender, EventArgs e)
+		{
+			//User has clicked the sign up button
+			mOnSignInComplete.Invoke(this, new OnSignInEventArgs(mTxtUserName.Text, mTxtPassword.Text));
+			this.Dismiss();
+		}
+
+		public override void OnActivityCreated(Bundle savedInstanceState)
+		{
+			Dialog.Window.RequestFeature(WindowFeatures.NoTitle); // Set the title bar to invisible.
+			base.OnActivityCreated(savedInstanceState);
+			Dialog.Window.Attributes.WindowAnimations = Resource.Style.dialog_animation;  // Set the animation.
+		}
+	}
+}
+
